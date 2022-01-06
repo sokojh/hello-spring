1f86bff94676e2b7d9b089a045a98bd7f0a824be
jiho kim <jihokim@jihoui-MacBookAir.local>
2022-01-06 10:52:05 +0900

    init

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.w3c.dom.CDATASection;
 
 @Controller
@@ -11,7 +12,14 @@ public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data","hello");
+        model.addAttribute("data","hello!!");
         return "hello";
     }
-}
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    }
+
